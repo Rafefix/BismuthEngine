487c53b69aea48a2c31d480d18456a67ff6ca608
FBarnes99 <36264879+FBarnes99@users.noreply.github.com>
2019-10-10 12:26:00 +0200

    Minor

--- BismuthEngine/ModuleScene.cpp
+++ BismuthEngine/ModuleScene.cpp
@@ -1,7 +1,7 @@
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleScene.h"
-#include "NewPrimitives.h"
+//#include "NewPrimitives.h"
 #include "ModuleRenderer3D.h"
 #include "math.h"
 
