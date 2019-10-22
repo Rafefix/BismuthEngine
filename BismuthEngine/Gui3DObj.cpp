f2eb2e19c19ff6f268121a9dcb72605804fc4889
FBarnes99 <36264879+FBarnes99@users.noreply.github.com>
2019-10-22 12:46:04 +0200

    minor

--- BismuthEngine/Gui3DObj.cpp
+++ BismuthEngine/Gui3DObj.cpp
@@ -25,7 +25,22 @@ void Gui3DOBJ::Draw() {
 	ImGui::SetNextWindowPos(ImVec2(200, 18), ImGuiCond_Once);
 	ImGui::SetNextWindowSize(ImVec2((float)window_width / 2.5, (float)window_height / 2), ImGuiCond_Once);
 
+
 	if (ImGui::Begin("3D Objects", &visible)) {
+		
+		/*if (ImGui::CollapsingHeader("Position")) {
+			ImGui::SliderInt("X", position.x, -25, 25);
+			ImGui::SliderInt("Y", &, -25, 25);
+			ImGui::SliderInt("Z", &, -25, 25);
+		}
+
+		
+		if (ImGui::CollapsingHeader("Transformations")) {
+			ImGui::SliderInt("Width", &, 1, 25);
+			ImGui::SliderInt("Height", &, 1, 25);
+			ImGui::SliderInt("Depth", &, 1, 25);
+		}*/
+
 		if (ImGui::Button("CUBE")) { App->scene->createShape(SHAPE::CUBE, vec3(0, 0, 0), vec3(1, 1, 1)); }
 		if (ImGui::Button("SPHERE")) { App->scene->createShape(SHAPE::SPHERE, vec3(0, 0, 0), vec3(1, 1, 1)); }
 		if (ImGui::Button("OCTOHEDRON")) { App->scene->createShape(SHAPE::OCTOHEDRON, vec3(0, 0, 0), vec3(1, 1, 1)); }
